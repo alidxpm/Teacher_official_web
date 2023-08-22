c82f0dcb909d45897b3bc67aa6570c9a7d9837b2
ali <117563531+alidxpm@users.noreply.github.com>
2023-08-22 16:27:48 +0800

    Update newsnotice.js

--- app/component/newsnotice.js
+++ app/component/newsnotice.js
@@ -1,8 +1,31 @@
 import styles from '../styles/newsnotice.module.css'
+import { useState, useEffect } from 'react';
 export default function Newsnotice(){
+     const [data, setdata] = useState(null);
+  useEffect(() => { 
+    fetch("https://hdums.hdu.edu.cn/gw/api/newsnotice")
+    .then(response=>response.json())
+    .then(response=>
+      {
+      setdata(response.data),
+      console.log("fetch_data",response.data)
+        } 
+      )}, []);
     return(
         <div className={styles.content}>
             <div className={styles.content1}>
+                <div className={styles.items1}>
+                    {(data||[]).map((item)=>{
+                      return(
+                      <ul>
+                        <li key={item.id}>{item.content}</li>
+                      </ul>
+                      )
+                  })}
+                </div>
+                <div className={styles.items2}>
+                    
+                </div>
             </div>
             <div className={styles.content2}>
             </div>
