5ed08f4de3a5faf9524d58b505a144de77050596
ali <117563531+alidxpm@users.noreply.github.com>
2023-08-22 16:29:23 +0800

    Update newshdu.js

--- app/component/newshdu.js
+++ app/component/newshdu.js
@@ -1,8 +1,31 @@
 import styles from '../styles/newshdu.module.css'
+import { useState, useEffect } from 'react';
 export default function Newshdu(){
+    const [data, setdata] = useState(null);
+  useEffect(() => { 
+    fetch("https://hdums.hdu.edu.cn/gw/api/newshdu")
+    .then(response=>response.json())
+    .then(response=>
+      {
+      setdata(response.data),
+      console.log("fetch_data",response.data)
+        } 
+      )}, []);
     return(
         <div className={styles.content}>
             <div className={styles.content1}>
+                <div className={styles.items1}>
+                    {(data||[]).map((item)=>{
+                      return(
+                      <ul>
+                        <li key={item.id}>{item.content}</li>
+                      </ul>
+                      )
+                  })}
+                </div>
+                <div className={styles.items2}>
+                    
+                </div>
             </div>
             <div className={styles.content2}>
             </div>
